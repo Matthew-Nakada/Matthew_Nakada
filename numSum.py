971e913d1b173d0208eebc698e12a704322fcb8e
SANDHILLS\LH218-19 <jad.talbert@gmail.com>
2025-08-27 10:34:53 -0400

    A

---
+++ numSum.py
@@ -0,0 +1,9 @@
+num1 = input("Pick a number: ")
+num2 = input("Pick another number: ")
+sum = float(num1) + float(num2)
+print("The sum of {0} and {1} is {2}".format(num1, num2, sum))
+
+num3 = input("Pick yet another number: ")
+num4 = input("Do not NOT pick a number: ")
+diff = float(num4) - float(num3)
+print("The difference between {0} and {1} is {2}".format(num4, num3, diff))
