0e02f5758be7af43a51723be05bc0b1083c12c09
SANDHILLS\LH218-19 <jad.talbert@gmail.com>
2025-09-03 10:31:34 -0400

    Yey

---
+++ squareRoot.py
@@ -0,0 +1,6 @@
+num = 5
+
+# ** Raises by a power
+square_root = num ** 5
+
+print(square_root)
---
+++ week3Lab.py
@@ -0,0 +1,19 @@
+#1.
+v = 80
+t = 2
+print("You travelled {0} miles.".format(v*t))
+
+#2.
+v = 400
+t = 4
+print("You travelled {0} miles.".format(v*t))
+
+#3.
+I = 10
+R = 2
+print("There's {0} volts.".format(I*R))
+
+#4. 
+m = 10.0
+a = 9.81**2
+print("The force is {0}".format(m*a))
